ceddf595137b34ee26593645fae99d46664d79d2
Xander <xanxerus@gmail.com>
2016-12-16 23:29:17 -0600

    Added a pitiful attempt at gradient descent

--- somnkarta.js
+++ somnkarta.js
@@ -1,108 +1,103 @@
-var MS_PER_MINUTE = 60000;
-var DESIRED_EPSILON = MS_PER_MINUTE/2;
-var J_UNIX_EPOCH = 2440587.5;
-var MS_PER_DAY = 86400000;
-
-var divname = "test-div"
 var date = new Date(); //Date.UTC(2016, 11, 14));
 var duration = 8*60*MS_PER_MINUTE;
 var latitude = 32.989924;
 var longitude = -96.751620;
+//~ var latitude = 0;
+//~ var longitude = 0;
+var div = null;
+
+function f2c( percentage ) {
+    var color_part_dec = Math.abs(Math.round(255 * percentage));
+    var color_part_hex = Number(color_part_dec).toString(16);
+    if(color_part_hex.length < 2)
+		color_part_hex = '0' + color_part_hex;
+    return "#" + color_part_hex + color_part_hex + color_part_hex;
+}
 
 function showThings(){
-	var div = document.getElementById(divname);
-	var m = riseset(latitude, longitude, date);
+	div = document.getElementById("test-div");
+	var m = riseset(date, longitude, latitude);
+	div.innerHTML += "Longitude: " + longitude + "<br>";
+	div.innerHTML += "Latitude: " + latitude + "<br>";
 	div.innerHTML += "Rise: " + m.rise + "<br>";
 	div.innerHTML += "Set: " + m.set + "<br>";
-
-	//~ for(var hour = -23; hour < 24; hour++){
-		//~ var m = riseset(latitude, longitude, new Date(2016, 11, 14, hour, 0, 0, 0));
-		//~ div.innerHTML += "Hour: " + hour + "    ";
-		//~ div.innerHTML += "Set: " + m.set + "    ";
-		//~ div.innerHTML += "Rise: " + m.rise + "<br>";
-	//~ }
+	difftest(date, m.set, m.rise);
+	//~ rstest(date, m.set, m.rise);
+	//~ qtest(date, m.set, m.rise);
+	//~ gradient_descent(date, m.set, m.rise);
 }
 
-//https://en.wikipedia.org/wiki/Sunrise_equation
-function riseset(date, latitude, longitude){
-	date = new Date(date.getFullYear(), date.getMonth(), date.getDate());
-	var n = Math.floor(date/MS_PER_DAY) + J_UNIX_EPOCH - 2451545.0008;
-	var J_star = n - longitude/360;
-	var M = (357.5291 + 0.98560028*J_star) % 360;
-	var M_radians = M / 180 * Math.PI;
-	var C = 1.9148*Math.sin(M_radians) + 0.0200*Math.sin(2*M_radians) + 0.0003*Math.sin(3*M_radians);
-	var L = (M+C+180+102.9372)%360;
-	var J_transit = 2451545.5 + J_star + 0.0053*Math.sin(M_radians) - 0.0069*Math.sin(2*L);
-	var sind = Math.sin(L/180*Math.PI)*Math.sin(23.44/180*Math.PI);
-	var cosw = ( Math.sin(-0.83/180*Math.PI) - Math.sin(latitude/180*Math.PI)*sind ) / (Math.cos(latitude/180*Math.PI)*Math.cos(Math.asin(sind)));
-	var w = Math.acos(cosw)*180/Math.PI;
-	
-	var J_set = J_transit + w/360;
-	var J_rise = J_transit - w/360;
-	
-	var date_set = new Date(MS_PER_DAY*(J_set - J_UNIX_EPOCH));
-	var date_rise = new Date(MS_PER_DAY*(J_rise - J_UNIX_EPOCH));
-	
-	return {'set':date_set, 'rise':date_rise};
-}
-
-function A(date, latitude, longitude){
-	date = new Date(date.getFullYear(), date.getMonth(), date.getDate());
-	var n = Math.floor(date/MS_PER_DAY) + J_UNIX_EPOCH - 2451545.0008;
-	var J_star = n - longitude/360;
-	var M = (357.5291 + 0.98560028*J_star) % 360;
-	var M_radians = M / 180 * Math.PI;
-	var C = 1.9148*Math.sin(M_radians) + 0.0200*Math.sin(2*M_radians) + 0.0003*Math.sin(3*M_radians);
-	var L = (M+C+180+102.9372)%360;
-	var J_transit = 2451545.5 + J_star + 0.0053*Math.sin(M_radians) - 0.0069*Math.sin(2*L);
-	var sind = Math.sin(L/180*Math.PI)*Math.sin(23.44/180*Math.PI);
-	var cosw = ( Math.sin(-0.83/180*Math.PI) - Math.sin(latitude/180*Math.PI)*sind ) / (Math.cos(latitude/180*Math.PI)*Math.cos(Math.asin(sind)));
-	var w = Math.acos(cosw)*180/Math.PI;
-	
-	var J_rise = J_transit - w/360;
-	
-	J_star -= 1;
-	M = (357.5291 + 0.98560028*J_star) % 360;
-	M_radians = M / 180 * Math.PI;
-	C = 1.9148*Math.sin(M_radians) + 0.0200*Math.sin(2*M_radians) + 0.0003*Math.sin(3*M_radians);
-	L = (M+C+180+102.9372)%360;
-	J_transit = 2451545.5 + J_star + 0.0053*Math.sin(M_radians) - 0.0069*Math.sin(2*L);
-	sind = Math.sin(L/180*Math.PI)*Math.sin(23.44/180*Math.PI);
-	cosw = ( Math.sin(-0.83/180*Math.PI) - Math.sin(latitude/180*Math.PI)*sind ) / (Math.cos(latitude/180*Math.PI)*Math.cos(Math.asin(sind)));
-	w = Math.acos(cosw)*180/Math.PI;
-
-	
-	var J_set = J_transit + w/360;
-
-	return (J_rise - J_set)*MS_PER_DAY;
-}
-
-function latitudeSearch(date, duration, longitude=0, div=null){
-	if(div != null)
-		div.innerHTML += "Searching latitudes for duration: " + duration + "<br>";
-
-	var southern = -90;
-	var northern = 90;
-	var increasing = A(date, -10, longitude) < A(date, 10, longitude); //never fails
-	var pot;
-
-	while(southern < northern){
-		pot = (southern+northern)/2;
-		a = A(date, pot, longitude);
-		if(div != null)
-			div.innerHTML += "Date: " + date + " Latitude: " + pot 
-						  + " Longitude: " + longitude + " Duration: " + a + "<br>";
-		
-		if(a != undefined && Math.abs(a - duration) < DESIRED_EPSILON)
-			return pot;
-		
-		if(a < duration && increasing || a > duration && !increasing || a == undefined && pot < 0)
-			southern = pot;
-		else if(a > duration && increasing || a < duration && !increasing || a==undefined && pot > 0)
-			northern = pot;
+function difftest(n, s, r){
+	setN(n);
+	setS(s);
+	setR(r);
+	div.innerHTML += jtog(getS());
+	var buffer = "<table>"
+	for(var phi = 90; phi >= -90; phi-=5){
+		buffer += "<tr>";
+		for(var l = -180; l <= 180; l+=5){
+			S1 = S(l, phi);
+			R1 = R(l, phi);
+			Q1 = Q(l, phi);
+			var v = Q1; //Math.abs(getS() - S(l, phi))/1.5;
+			buffer += "<td bgcolor=\"" + "white" + "\">" + (Q_phi(l, phi)<0?'v':'^') +  "</td>";
+		}
+		buffer += "</tr>";
 	}
-
-	if(div != null)
-		div.innerHTML += "For some reason, no latitude was found.<br>";
-
+	buffer += "</table>";
+	div.innerHTML += buffer;
 }
+
+
+function rstest(n, s, r){
+	setN(n);
+	setS(s);
+	setR(r);
+	var buffer = "<table>"
+	for(var phi = 90; phi >= -90; phi-=5){
+		buffer += "<tr>";
+		for(var l = -180; l <= 180; l+=5){
+			buffer += "<td>" + jtog(R(l, phi)).getHours() + ", " + jtog(S(l, phi)).getHours() + "</td>";
+		}
+		buffer += "</tr>";
+	}
+	buffer += "</table>";
+	div.innerHTML += buffer;
+}
+
+
+function qtest(n, s, r){
+	setN(n);
+	setS(s);
+	setR(r);
+	var buffer = "<table>"
+	for(var phi = -90; phi <= 90; phi+=5){
+		buffer += "<tr>";
+		for(var l = -180; l <= 180; l+=5){
+			buffer += "<td>" + Q(l, phi) + "</td>";
+		}
+		buffer += "</tr>";
+	}
+	buffer += "</table>";
+	div.innerHTML += buffer;
+}
+
+function gradient_descent(n, s, r){
+	setN(n);
+	setS(s);
+	setR(r);
+	var l = 0
+	var phi = 0;
+	var buffer = "<table><tr><td width=\"15%\">Longitude</td><td width=\"15%\">Latitude</td><td width=\"25%\">Sunrise</td><td width=\"25%\">Sunset</td><td width=\"15%\">Q</td></tr>";
+	
+	while(Q(l, phi) > 1){
+		buffer += "<tr><td>" + l + "</td><td>" + phi  + "</td><td>" + jtog(R(l, phi))  + "</td><td>" +  jtog(S(l, phi))  + "</td><td>" +  Q(l, phi) + "</td></tr>";
+		l = (l + Q_l(l, phi));// % 180;
+		phi = (phi + Q_phi(l, phi));// % 90;
+	}
+	buffer += "<tr><td>" + l + "</td><td>" + phi  + "</td><td>" + jtog(R(l, phi))  + "</td><td>" +  jtog(S(l, phi))  + "</td><td>" +  Q(l, phi) + "</td></tr></table>";
+	
+	div.innerHTML += buffer;
+}
+
+
--- sun.js
+++ sun.js
@@ -1,1 +1,183 @@
+var MS_PER_MINUTE = 60000;
+var J_UNIX_EPOCH = 2440587.5;
+var MS_PER_DAY = 86400000;
 
+var n = -1;
+var s = -1;
+var r = -1;
+var c1 = 357.5291;
+var c2 = 0.98560028;
+var c3 = 1.9148;
+var c4 = 0.0200;
+var c5 = 0.0003;
+var c6 = 282.9372;
+var c7 = 2451545.5;
+var c8 = 0.0053;
+var c9 = 0.0069;
+var c10 = Math.sin(23.44 * Math.PI / 180);
+var c11 = Math.sin(-0.83 * Math.PI / 180);
+
+//https://en.wikipedia.org/wiki/Sunrise_equation
+//l is longitude, phi is latitude
+function riseset(date, l, phi){
+	setN(date);
+	return {'set':jtog(S(l, phi)), 'rise':jtog(R(l, phi))};
+}
+
+function jtog(j){
+	return new Date(MS_PER_DAY*(j - J_UNIX_EPOCH))
+}
+
+function gtoj(g){
+	return J_UNIX_EPOCH + g / MS_PER_DAY;
+}
+
+function floorgtoj(g){
+	return J_UNIX_EPOCH + Math.floor(floordate(g) / MS_PER_DAY);
+}
+
+function floordate(g){
+	return new Date(g.getFullYear(), g.getMonth(), g.getDate());
+}
+
+function setN(given){
+	n = floorgtoj(given) - 2451545.0008;
+}
+
+function setS(given){
+	s = gtoj(given);
+}
+
+function setR(given){
+	r = gtoj(given);
+}
+
+function getS(){
+	return s;
+}
+function getR(){
+	return r;
+}
+function getN(){
+	return n;
+}
+
+function M(l){
+	//~ document.getElementById("test-div").innerHTML = n + "<br>";
+	return (c1 + c2*(n - l/360)) % 360 * Math.PI / 180;
+}
+
+function C(l){
+	return c3*Math.sin(M(l)) + c4*Math.sin(2*M(l)) + c5*Math.sin(3*M(l));
+}
+
+function L(l){
+	return (M(l) + C(l) + c6) % 360 * Math.PI / 180;
+}
+
+function J(l){
+	return c7 + n - l/360 + c8*Math.sin(M(l)) - c9*Math.sin(2*L(l));
+}
+
+function Delta(l){
+	return Math.asin(c10*Math.sin(L(l)));
+}
+
+function W(l, phi){
+	return Math.acos( (c11 - Math.sin(phi*Math.PI/180)*Math.sin(Delta(l))) / (Math.cos(phi*Math.PI/180)*Math.cos(Delta(l))) );
+}
+
+function S(l, phi){
+	return J(l) + W(l, phi)/(2*Math.PI);
+}
+
+function R(l, phi){
+	return J(l) - W(l, phi)/(2*Math.PI);
+}
+
+function Q(l, phi){
+	return (Math.pow(S(l, phi)-s, 2)+1) * (Math.pow(R(l, phi)-r, 2)+1);
+}
+
+function Q_l(l, phi){
+	return Math.pow(S(l, phi)-s, 2) * 2 * R_l(l, phi) * (R(l, phi) - r)
+		+  Math.pow(R(l, phi)-r, 2) * 2 * S_l(l, phi) * (S(l, phi) - s)
+		+  2 * S_l(l, phi) * (S(l, phi) - s)
+		+  2 * R_l(l, phi) * (R(l, phi) - r);
+}
+
+function Q_phi(l, phi){
+	return Math.pow(S(l, phi)-s, 2) * 2 * R_phi(l, phi) * (R(l, phi) - r)
+		+  Math.pow(R(l, phi)-r, 2) * 2 * S_phi(l, phi) * (S(l, phi) - s)
+		+  2 * S_phi(l, phi) * (S(l, phi) - s)
+		+  2 * R_phi(l, phi) * (R(l, phi) - r);
+}
+
+
+function M_l(l){
+	return -c2/360* Math.PI / 180;
+}
+
+function C_l(l){
+	return c3*M_l(l)*cos(M(l)) + c4*2*M_l(l)*Math.cos(2*M(l)) + c5*3*M_l(l)*cos(3*M(l));
+}
+
+function L_l(l){
+	return M_l(l) + (C_l(l) * Math.PI / 180);
+}
+
+function J_l(l){
+	return -1/360 + c8*M_l(l)*Math.cos(M(l)) - c9*2*L_l(l)*Math.cos(2*L(l));
+}
+
+function Delta_l(l){
+	return U_l(l) / Math.sqrt(1 - Math.pow(U(l), 2));
+}
+
+function U(l){
+	return c10 * sin(L(l));
+}
+
+function U_l(l){
+	return c1 * L_l(l) * Math.cos(L(l));
+}
+
+function W_l(l){
+	return -V_l(l) / Math.sqrt(1 - Math.pow(V(l), 2));
+}
+
+function V(l, phi){
+	return (c11 - Math.sin(phi)*Math.sin(Delta(l))) / (Math.cos(phi)*Math.cos(Delta(l)));
+}
+
+function V_l(l, phi){
+	return Delta_l(l) * (Math.cos(phi) * Math.cos(Delta(l)) * (-Math.sin(phi)) * Math.cos(Delta(l)) 
+		+  (c11-Math.sin(phi)*Math.sin(Delta(l)))*Math.cos(phi)*Math.sin(Delta(l)))
+	 / Math.pow(Math.cos(phi)*Math.cos(Delta(l)), 2);
+}
+
+function S_l(l, phi){
+	return J_l(l) + W_l(l) / 360;
+}
+
+function R_l(l, phi){
+	return J_l(l) - W_l(l) / 360;
+}
+
+function S_phi(l, phi){
+	return W_phi(l, phi) / 360;
+}
+
+function R_phi(l, phi){
+	return - W_phi(l, phi) / 360;
+}
+
+function W_phi(l, phi){
+	return - V_phi(l, phi) / Math.sqrt(1 - Math.pow(V(l, phi), 2));
+}
+
+function V_phi(l, phi){
+	return (Math.cos(phi) * Math.cos(Delta(l)) * (-Math.cos(phi)) * Math.sin(Delta(l)) 
+		+  (c11-Math.sin(phi)*Math.sin(Delta(l)))*(-Math.sin(phi))*Math.cos(Delta(l)))
+	 / Math.pow(Math.cos(phi)*Math.cos(Delta(l)), 2);
+}
